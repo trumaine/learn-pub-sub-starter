5a281135907a4a8d2c6277fdf600e836ba3de94b
= <=>
2026-06-24 12:18:12 -0400

    ch3, l9: Durable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,6 +27,18 @@ func main() {
 	}
 	defer publishCh.Close()
 
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug+".*",
+		pubsub.SimpleQueueDurable,
+	)
+	if err != nil {
+		log.Fatalf("could not subscribe to game_log: %v", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
 	gamelogic.PrintServerHelp()
 
 	for {
